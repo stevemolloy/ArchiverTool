045690c36df41ac2c16968f2bb7cf2243d6477e3
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2019-02-27 15:55:30 +0100

    Sorta concurrent

--- archiver_tool.py
+++ archiver_tool.py
@@ -1,14 +1,13 @@
 from argparse import ArgumentParser
 import requests
 import json
+import asyncio
+from functools import partial
 
 BASEURL = 'http://control.maxiv.lu.se/general/archiving/'
 SEARCHURL = BASEURL + 'search'
 QUERYURL = BASEURL + 'query'
 CONTROLURL = "g-v-csdb-0.maxiv.lu.se:10000"
-
-payload = {'targets': [{'target': "r1-d111210cab01/mag/psib-12/current", 'cs': "g-v-csdb-0.maxiv.lu.se:10000"}], 'range': {'from': "2018-06-16T15:00:00", 'to': "2018-06-16T15:30:00"}}
-payload = {'cs': 'g-v-csdb-0.maxiv.lu.se:10000', 'target': 'r1.*cab01.*current'}
 
 def makesearchpayload(searchterm):
     return {
@@ -22,25 +21,57 @@ def makequerypayload(signal, start, end):
             'range': {'from': start, 'to': end,}
             }
 
+def parse_response(resp):
+    return json.loads(resp.text)
+
+@asyncio.coroutine
+def do_request(args):
+    loop = asyncio.get_event_loop()
+    payload = makequerypayload(args.query, args.start, args.end)
+    future0 = loop.run_in_executor(
+            None,
+            partial(requests.post, url=QUERYURL, json=payload),
+            )
+    future1 = loop.run_in_executor(
+            None,
+            partial(requests.post, url=QUERYURL, json=payload),
+            )
+    response0 = yield from future0
+    response1 = yield from future1
+    return [response0, response1]
+
 if __name__=="__main__":
     parser = ArgumentParser(description='Get data from HDB++ archiver')
-    parser.add_argument('query', type=str, default='', help='Signal to acquire')
-    parser.add_argument('start', type=str, default='', help='Start')
-    parser.add_argument('end', type=str, default='', help='End')
     parser.add_argument(
-            '-s',
-            '--searchterm',
+            'signals',
             type=str,
-            help='Search term (regex)',
+            nargs='+',
+            help='Signal(s) to acquire',
+            )
+    parser.add_argument(
+            '-s', '--start',
+            type=str,
+            required=True,
+            help='Start of time-range',
+            )
+    parser.add_argument(
+            '-e', '--end',
+            type=str,
+            required=True,
+            help='End of time-range'
             )
 
     args = parser.parse_args()
 
-    r = requests.post(
-            url=QUERYURL,
-            json=makequerypayload(args.query, args.start, args.end)
-            )
-    print(json.loads(r.text))
+    loop = asyncio.get_event_loop()
+    response = loop.run_until_complete(do_request(args))
+    print([parse_response(resp) for resp in response])
+
+    # r = requests.post(
+    #         url=QUERYURL,
+    #         json=makequerypayload(args.query, args.start, args.end)
+    #         )
+    # print(json.loads(r.text))
 
     if args.searchterm is not None:
         r = requests.post(
